efab6c70222ebaeba8e703ab3e9bc26872b6ca40
leo.daunbi <leothai.daunbi@gmail.com>
2023-09-08 12:50:05 +0900

    success

--- admin/resources/views/schedule/edit.blade.php
+++ admin/resources/views/schedule/edit.blade.php
@@ -20,18 +20,14 @@
 
       <!-- Container to Append New Checkboxes -->
       <div id="additionalCheckboxes">
-         <!-- Existing checkboxes for button1 and button2 -->
-         <div class="checkbox-container" id="checkbox1">
-            <input type="checkbox" id="button1" name="button1"{{ $schedule->button1 ? 'checked' : '' }}>
-            <label for="button1">ゆ</label>
-            <button type="button" class="removeCheckboxButton" onclick="removeCheckbox('checkbox1')">X</button>
+         <!-- Existing checkboxes for students -->
+         @foreach($schedule->students as $student)
+         <div class="checkbox-container" id="checkbox{{ $student->id }}">
+            <input type="checkbox" id="button{{ $student->id }}" name="students[]" value="{{ $student->name }}" checked>
+            <label for="button{{ $student->id }}">{{ $student->name }}</label>
+            <button type="button" class="removeCheckboxButton" onclick="removeCheckbox('checkbox{{ $student->id }}')">X</button>
          </div>
-         
-         <div class="checkbox-container" id="checkbox2">
-            <input type="checkbox" id="button2" name="button2"{{ $schedule->button2 ? 'checked' : '' }}>
-            <label for="button2">う</label>
-            <button type="button" class="removeCheckboxButton" onclick="removeCheckbox('checkbox2')">X</button>
-         </div>
+         @endforeach
       </div>
 
       <!-- Submit Button -->
@@ -40,88 +36,65 @@
 
     <!-- Add JavaScript code to submit the form and handle adding/removing checkboxes -->
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const form = document.querySelector('form');
-            const addCheckboxButton = document.getElementById('addCheckboxButton');
-            const additionalCheckboxes = document.getElementById('additionalCheckboxes');
+    function removeCheckbox(checkboxContainerId) {
+        const checkboxContainer = document.getElementById(checkboxContainerId);
+        if (checkboxContainer) {
+            checkboxContainer.remove();
+        }
+    }
 
-            addCheckboxButton.addEventListener('click', function () {
-                const checkboxLabel = prompt('Enter checkbox label:');
-                if (checkboxLabel) {
-                    const checkboxId = 'button' + (additionalCheckboxes.children.length + 1); // Increment the checkbox id
+    document.addEventListener('DOMContentLoaded', function () {
+        const form = document.querySelector('form');
+        const addCheckboxButton = document.getElementById('addCheckboxButton');
+        const additionalCheckboxes = document.getElementById('additionalCheckboxes');
 
-                    // Create a new checkbox container
-                    const checkboxContainer = document.createElement('div');
-                    checkboxContainer.className = 'checkbox-container';
-                    checkboxContainer.id = 'checkbox' + (additionalCheckboxes.children.length + 1);
+        addCheckboxButton.addEventListener('click', function () {
+            const checkboxLabel = prompt('Enter student name:');
+            if (checkboxLabel) {
+                const checkboxId = 'button' + (additionalCheckboxes.children.length + 1); // Increment the checkbox id
 
-                    // Create a checkbox
-                    const checkbox = document.createElement('input');
-                    checkbox.type = 'checkbox';
-                    checkbox.id = checkboxId;
-                    checkbox.name = checkboxId;
-                    checkbox.value = '1';
+                // Create a new checkbox container
+                const checkboxContainer = document.createElement('div');
+                checkboxContainer.className = 'checkbox-container';
+                checkboxContainer.id = 'checkbox' + (additionalCheckboxes.children.length + 1);
 
-                    // Create a label
-                    const label = document.createElement('label');
-                    label.setAttribute('for', checkboxId);
-                    label.textContent = checkboxLabel;
+                // Create a checkbox
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.id = checkboxId;
+                checkbox.name = 'students[]';
+                checkbox.value = checkboxLabel;
+                checkbox.checked = true;
 
-                    // Create a remove button
-                    const removeButton = document.createElement('button');
-                    removeButton.type = 'button';
-                    removeButton.className = 'removeCheckboxButton';
-                    removeButton.textContent = 'X';
-                    removeButton.onclick = function () {
-                        removeCheckbox(checkboxContainer.id);
-                    };
+                // Create a label
+                const label = document.createElement('label');
+                label.setAttribute('for', checkboxId);
+                label.textContent = checkboxLabel;
 
-                    // Append elements to the checkbox container
-                    checkboxContainer.appendChild(checkbox);
-                    checkboxContainer.appendChild(label);
-                    checkboxContainer.appendChild(removeButton);
+                // Create a remove button
+                const removeButton = document.createElement('button');
+                removeButton.type = 'button';
+                removeButton.className = 'removeCheckboxButton';
+                removeButton.textContent = 'X';
+                removeButton.onclick = function () {
+                    removeCheckbox(checkboxContainer.id);
+                };
 
-                    // Add the new checkbox container to the "additionalCheckboxes" div
-                    additionalCheckboxes.appendChild(checkboxContainer);
-                }
-            });
+                // Append elements to the checkbox container
+                checkboxContainer.appendChild(checkbox);
+                checkboxContainer.appendChild(label);
+                checkboxContainer.appendChild(removeButton);
 
-            form.addEventListener('submit', function (event) {
-                // Set the values of checkboxes based on their checked state
-                const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-                checkboxes.forEach(function (checkbox) {
-                    document.querySelector('input[name="' + checkbox.name + '"]').value = checkbox.checked ? '1' : '0';
-                });
-            });
-
-            fetch('/add-student', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-            },
-            body: JSON.stringify({
-                label: checkboxLabel,
-                // Add other data you want to send to the server
-                })
-            })
-            .then(response => response.json())
-            .then(data => {
-                // Handle the response, e.g., show a success message
-                console.log(data);
-            })
-            .catch(error => {
-                // Handle errors
-                console.error(error);
-            });
+                // Add the new checkbox container to the "additionalCheckboxes" div
+                additionalCheckboxes.appendChild(checkboxContainer);
+            }
         });
 
-        function removeCheckbox(checkboxContainerId) {
-            const checkboxContainer = document.getElementById(checkboxContainerId);
-            if (checkboxContainer) {
-                checkboxContainer.remove();
-            }
-        }
+        form.addEventListener('submit', function (event) {
+            // Form submission will handle the addition and removal of checkboxes
+            // No need for additional JavaScript here
+        });
+        });
     </script>
 </body>
 </html>
