cdcaaaaccd41d70311baaec56f723f36ad9b417e
leo.daunbi <leothai.daunbi@gmail.com>
2023-09-15 01:38:29 +0900

    nav

--- laravel/resources/views/schedule/index.blade.php
+++ laravel/resources/views/schedule/index.blade.php
@@ -1,62 +1,19 @@
-
+@extends('layouts.app')
 
 @section('content')
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <!-- 네비게이션 바 코드 -->
+    
+    <!-- 새로운 뷰의 내용을 추가하세요 -->
+    
+    @push('styles')
+        <!-- 여기에 해당 뷰에서만 필요한 스타일 코드를 추가하세요 -->
+    @endpush
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="active"><a href="{{ route('home')}}" class="float-right">Blog</a></li>
-                        <li class="active"><a href="{{ url('/schedule') }}" class="float-right">Schedule</a></li>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
+    @push('scripts')
+        <!-- 여기에 해당 뷰에서만 필요한 스크립트 코드를 추가하세요 -->
+    @endpush
 @endsection
+
 
 <!DOCTYPE html>
 <html lang='en'>
